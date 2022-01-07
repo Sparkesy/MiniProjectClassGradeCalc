e0fb56d5dd9bcc84308ac35c2542f8452ed46bc8
Jordan Sparkes <Jordan.sparkes@jsmsdn.onmicrosoft.com>
2022-01-07 09:48:41 +0000

    all done

--- MiniProjectClassGradeCalc/Form1.Designer.cs
+++ MiniProjectClassGradeCalc/Form1.Designer.cs
@@ -29,13 +29,181 @@ namespace MiniProjectClassGradeCalc
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.LSTresults = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.calculateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.NUD_G1 = new System.Windows.Forms.NumericUpDown();
+            this.NUD_G3 = new System.Windows.Forms.NumericUpDown();
+            this.NUD_G2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_G1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_G3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_G2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LSTresults
+            // 
+            this.LSTresults.FormattingEnabled = true;
+            this.LSTresults.Location = new System.Drawing.Point(10, 260);
+            this.LSTresults.Name = "LSTresults";
+            this.LSTresults.Size = new System.Drawing.Size(573, 173);
+            this.LSTresults.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculateToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(592, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // calculateToolStripMenuItem
+            // 
+            this.calculateToolStripMenuItem.Name = "calculateToolStripMenuItem";
+            this.calculateToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
+            this.calculateToolStripMenuItem.Text = "Calculate";
+            this.calculateToolStripMenuItem.Click += new System.EventHandler(this.calculateToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(98, 147);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(180, 20);
+            this.txt_name.TabIndex = 2;
+            // 
+            // NUD_G1
+            // 
+            this.NUD_G1.Location = new System.Drawing.Point(356, 110);
+            this.NUD_G1.Name = "NUD_G1";
+            this.NUD_G1.Size = new System.Drawing.Size(120, 20);
+            this.NUD_G1.TabIndex = 3;
+            // 
+            // NUD_G3
+            // 
+            this.NUD_G3.Location = new System.Drawing.Point(356, 190);
+            this.NUD_G3.Name = "NUD_G3";
+            this.NUD_G3.Size = new System.Drawing.Size(120, 20);
+            this.NUD_G3.TabIndex = 4;
+            // 
+            // NUD_G2
+            // 
+            this.NUD_G2.Location = new System.Drawing.Point(356, 147);
+            this.NUD_G2.Name = "NUD_G2";
+            this.NUD_G2.Size = new System.Drawing.Size(120, 20);
+            this.NUD_G2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(175, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Grade 1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Grade 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Grade 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Orange;
+            this.label5.Location = new System.Drawing.Point(134, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(305, 49);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Grade Calculator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(592, 442);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NUD_G2);
+            this.Controls.Add(this.NUD_G3);
+            this.Controls.Add(this.NUD_G1);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.LSTresults);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Grade Calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_G1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_G3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NUD_G2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox LSTresults;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem calculateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.NumericUpDown NUD_G1;
+        private System.Windows.Forms.NumericUpDown NUD_G3;
+        private System.Windows.Forms.NumericUpDown NUD_G2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- MiniProjectClassGradeCalc/Form1.cs
+++ MiniProjectClassGradeCalc/Form1.cs
@@ -16,5 +16,67 @@ namespace MiniProjectClassGradeCalc
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void calculateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string name;
+            Decimal grade1;
+            Decimal grade2;
+            Decimal grade3;
+            Decimal total;
+            Decimal average;
+            string grade;
+
+            try
+            {
+                total = 0;
+                average = 0;
+                name = txt_name.Text;
+                grade1 = NUD_G1.Value;
+                grade2 = NUD_G2.Value;
+                grade3 = NUD_G3.Value;
+                total = grade1 + grade2 + grade3;
+                average = total / 3;
+                string LisItem;
+
+                if (average < 40)
+                {
+                    grade = "fail";
+                }
+                else if (average < 55)
+                {
+                    grade = "Pass";
+                }
+                else if (average < 70)
+                {
+                    grade = "Merit";
+                }
+                else
+                {
+                    grade = "Distinction";
+                }
+                LisItem = "Name = " + name + ", Mark = " + average.ToString() + ", Grade = " + grade;
+                LSTresults.Items.Add(LisItem);
+                txt_name.Clear();
+                NUD_G1.Value = 0;
+                NUD_G2.Value = 0;
+                NUD_G3.Value = 0;
+                txt_name.Focus();
+            }
+            catch
+            {
+                MessageBox.Show("Error!!!!! - Check your entries.");
+            }
+        }
     }
 }
